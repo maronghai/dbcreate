2757d9e09d585e522304363366e7a58d678d84a0
ronghai.ma <ronghai.ma@gmail.com>
2024-03-25 04:00:58 +0800

    format

--- test/f.js
+++ test/f.js
@@ -42,70 +42,70 @@ const commitTemplate = ctx => {
 
 }
 const commitTable = (ctx, index) => {
-	const parsing = ctx.parsing
-	let tableComment = parsing[0].sqlComment
-	tableComment = tableComment ? `\nCOMMENT = '` + tableComment + `'` : ''
+  const parsing = ctx.parsing
+  let tableComment = parsing[0].sqlComment
+  tableComment = tableComment ? `\nCOMMENT = '` + tableComment + `'` : ''
 
-	ctx.blocks.push({
-		index: parsing.parsing,
-		$index: index,
-		type: 'table',
-		src: parsing.map(line => line.src).join('\n') +  tableComment + '\n);\n'
-	})
-	ctx.parsing = null
+  ctx.blocks.push({
+    index: parsing.parsing,
+    $index: index,
+    type: 'table',
+    src: parsing.map(line => line.src).join('\n') + tableComment + '\n);\n'
+  })
+  ctx.parsing = null
 }
 
 const commitParsing = (ctx, index) => {
-	if ('table' == ctx.parsing[0].type) commitTable(ctx)
-	// if ('template' == ctx.parsing[0].type) commitTemplate(ctx)
+  if ('table' == ctx.parsing[0].type) commitTable(ctx)
+  // if ('template' == ctx.parsing[0].type) commitTemplate(ctx)
 }
 
 const parse = src => {
-	const lines = src.split(/\n/)
+  const lines = src.split(/\n/)
 
-	const step1 = lines.reduce((r, line, index) => {
-		const [a, b] = line
-		if (['$', '#'].includes(a) && r.parsing) {
-			commitParsing(r, index)
-		}
+  const step1 = lines.reduce((r, line, index) => {
+    const [a, b] = line
+    if (['$', '#'].includes(a) && r.parsing) {
+      commitParsing(r, index)
+    }
 
-		if (';' == a) {
-			r.blocks.push({
-				index,
-				type: 'spec_comment',
-				src: '-- ' + line.substring(1) + '\n'
-			})
-		}
+    if (';' == a) {
+      r.blocks.push({
+        index,
+        type: 'spec_comment',
+        src: '-- ' + line.substring(1) + '\n'
+      })
+    }
 
-		if ('$' == a) {
-			const token = /\$\s*(\w+)\s*(?:;\s*(.*))?/.exec(line)
-			const name = token[1]
-			const specComment = token[2] ? '  -- ' + token[2] : ''
-			r.blocks.push({
-				index,
-				type: 'spec_comment',
-				src: 'CREATE DATABASE `' + token[1] + '`' + specComment + ';'
-			})
-		}
-		if ('#' == a && '?' != b) {
-			const token = /#(\w*)\s*(\w+)\s*((?:\/\/|--).*?(?=\s+;|$))?(?:\s*;\s*(.*))?/.exec(line)
-			const [, template, table, sqlComment, specComment] = token
+    if ('$' == a) {
+      const token = /\$\s*(\w+)\s*(?:;\s*(.*))?/.exec(line)
+      const name = token[1]
+      const specComment = token[2] ? '  -- ' + token[2] : ''
+      r.blocks.push({
+        index,
+        type: 'spec_comment',
+        src: 'CREATE DATABASE `' + token[1] + '`' + specComment + ';'
+      })
+    }
+    if ('#' == a && '?' != b) {
+      const token = /#(\w*)\s*(\w+)\s*((?:\/\/|--).*?(?=\s+;|$))?(?:\s*;\s*(.*))?/.exec(line)
+      const [, template, table, sqlComment, specComment] = token
 
-			r.parsing = [{
-				index,
-				type: 'table',
-				src: 'CREATE TABLE `' + table + '` (' + __spec_comment(specComment),
-				template,
-				sqlComment,
-				specComment,
-			}]
-		}
-		return r
-	}, {
-		parsing: null,
-		blocks: []
-	})
-	console.log(step1)
+      r.parsing = [{
+        index,
+        type: 'table',
+        src: 'CREATE TABLE `' + table + '` (' + __spec_comment(specComment),
+        template,
+        sqlComment,
+        specComment,
+      }]
+    }
+    return r
+  }, {
+    parsing: null,
+    blocks: []
+  })
+  console.log(step1)
 }
 
 parse(s)
